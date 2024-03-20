4ec8b0fdf0658cf847a7c30d47b3d873f2876830
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2024-03-20 11:27:36 +0500

    ds

--- resources/views/website/services.blade.php
+++ resources/views/website/services.blade.php
@@ -10,24 +10,29 @@
                         <h2 class="mb-2 text-lg text-center pt-5">Our Services</h2>
                         <div class="row pt-5">
                             <div class="col-md-4 pt-5">
-                                <img src="{{url('images/our_mission.png')}}" alt="" class="img-fluid w-100">
+                                <img src="{{url('images/web-design.jpg')}}" alt="" class="img-fluid w-100  mt-5 pt-4">
                             </div>
                             <div class="col-md-8">
-                                <h3 class="text-center font-xl pt-3">Website Designing</h3>
-                                <p>
-                                    Our distinct mission at Rubicsol is to revolutionise the field of laboratory information management by providing a leading-edge RUBIC LIMS solution that meets the varied and changing needs of our clients in a range of lab sectors. Our dedication lies in promoting creativity and providing intuitive, adaptable, and safe software that boosts data handling, streamlines processes, and guarantees the best possible quality, security and precision of lab data.
-                                </p>
-                                <p>
-                                    Our objective is to create enduring relationships with our clients, comprehend their particular needs, and collaborate with them to transform their laboratory operations—rather than merely selling them a product. By consistently investing in research and development, we hope to remain at the forefront of technological breakthroughs and provide the most advanced LIMS solutions on the market.
-                                </p>
-                            </div>
-
+                            <h3 class="text-center font-xl pt-3">Website Designing</h3>
+                            <h5 class="text-center">A Glimpse into Rubicsol's Revolutionary Website Designs</h5>
+                            <p>The online cornerstone of your business is its website. It acts as your main point of contact with customers and a conversion tool. At Rubicsol, we pride ourselves on providing remarkable website designing services that go above and beyond the norm to maximise the reach of your online visibility to an exceptional level.</p>
+                            <!-- <p>We understand that every client is different, and their needs may vary. Our team of diligent professionals is committed to building websites that not only capture the attention of your target audience but also accurately represent your distinct brand identity.We work on fulfilling those needs with an uncompromising dedication to quality to create websites that are not only aesthetically beautiful but also effective at promoting business growth.</p> -->
+                            <p>With Rubicsol as your partner for website design, you can anticipate a smooth working relationship that produces an exceptional digital work of art. While working with us, you get:</p>
+                            <ul>
+                            <li><u>Personalised Designs:</u> Reflect your individuality and values with customisable web designs.</li>
+                            <li><u>Improved User Experience (UX):</u> Get easier navigation and increased engagement for your web.</li>
+                            <li><u>Constant Advancement:</u> Always stay ahead of the rapidly evolving needs of better web functionality and visuality.</li>
+                            <li><u>Time and Resource Management:</u> Minimise resources and manual work. Concentrate on your primary business operations and let us handle the headaches of a better website design.</li>
+                            <li><u>Post-launch Support:</u> Receive committed post-launch support and keep your web updated and well-maintained.</li>
+                            </ul>
+                            
+                             </div>
                         </div>
-
                     </div>
-                </div>
-            </div>
 
+                </div>
+
+            </div>
         </div>
     </section>
 <!--2nd section-->
@@ -37,19 +42,25 @@
             <div class="row mt-5">
                 <div class="col-lg-8">
                     <h3 class="text-center">Website Application Development</h3>
-                    <p>
-                        Rubicsol, a reputable software company, specialises in the comprehensive provision of RUBIC LIMS. Our persistent commitment to innovation and client-centric approach allows us to provide customised, secure, and user-friendly LIMS software.
-                    </p>
-                    <p>
-                        Through an emphasis on data quality, efficiency, and smooth integration, we enable our valued clients to optimise their laboratory processes, leading to automated Lab operations and overall increased output. We meticulously adhere to ISO/IEC 17025 standards keeping our clients stress-free for international recognition. Our dedication to quality, excellence and maintenance of long-time relationships with our clients has shaped our position as a leading player in the global LIMS marketplace.
-                    </p>
-                    <p>
-                        Discover the Rubicsol difference today and use our pioneering LIMS solution,the RUBIC LIMS to unlock the full potential of your lab. Become a part of our expanding clientele of happy customers and preserve your place in the cutthroat field of laboratory management.
-                    </p>
-                </div>
+                    <h5 class="text-center">Introducing Rubicsol's Web Application Development Expertise</h5>
+                    <p>With multiple completed web projects in our portfolio, Rubicsol is a leading web application development firm that specialises in web application development and stays current with online trends to produce simple to use, modern solutions.</p>
+                    <p>We believe every web application project is different and at Rubicsol, we provide our undivided attention to each and every specific detail for these projects.We take your vision and our deep industry experience to design custom web applications that not only solve your immediate challenges but also open up new opportunities for growth ahead. Our outstanding reputation in the industry is a result of our dedication to quality, innovation, and client satisfaction. </p>
+                    <p>Make Use of the Best Technologies and Latest Trends
+                     Embark on a journey like no other with Rubicsol's web application development services. Witness how our web applications can propel your business or idea to new heights with:</p>
+                        
+             <ul>
+                   <li>Responsive design</li>
+                   <li>Full-proof security</li>
+                   <li>Good user functionality</li>
+                   <li>Easy Accessibility</li>
+                   <li>Mobile-friendliness</li>
+
+                </ul>                
+
+</div>
 
                 <div class="col-lg-4">
-                    <img src="{{url('images/about_us.png')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
+                    <img src="{{url('images/web-app.jpg')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
                 </div>
             </div>
         </div>
@@ -63,22 +74,23 @@
             <div class="row mt-5">
 
                 <div class="col-lg-4">
-                    <img src="{{url('images/our_vision.png')}}" alt="" class="img-fluid w-100">
+                    <img src="{{url('images/android-ios.jpg')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
                 </div>
                 <div class="col-lg-8">
                     <h3 class="text-center">Mobile Application Android & iOS Development</h3>
-                    <p>
-
-                        We envision a world where laboratories of all sizes and specialisations seamlessly integrate our all-in-one software,RUBIC LIMS simplifying their operations, and opening up new avenues for development, cooperation, and innovation leading to technically advanced and quality assured outcomes.
-
-                    </p>
-                    <p>
-                        In order to inspire and empower a new generation of scientists, researchers, and professionals to design a brighter, more connected, and technologically sophisticated output for various sectors, we constantly push the frontiers of what is possible in laboratory information management to cope with the evolving needs of the world.
-                    </p>
-                    <p>
-                        Together, we envision a world whereby our clients are free to concentrate on their areas of expertise while we diligently manage the complexities of laboratory information management, clearing the way to
-                        victory for all!
-                    </p>
+                    <h5 class="text-center">Countless perks of using Rubicsol's mobile app development services</h5>
+                    <p>Rubicsol's mobile application development services for the iOS and Android platforms mainly focus on quality and creativity. We provide a simple development process for you to follow with an end product of an advanced, high-performing app suited to your needs.</p>
+                
+                    <ul>
+                
+                    <li><u>Individualised Brand Experience:</u> Make your app appealing to your target market and consistent with your brand identity.</li>
+                    <li><u>User-Centric Design:</u> Prioritise clear and effective user interfaces to boost user satisfaction and engagement.</li>
+                    <li><u>Proficiency in Up-to-Date Technologies:</u> Make use of the most recent developments in app development to provide novel solutions.</li>
+                    <li><u>Testing and Quality Assurance:</u> Via thorough testing and quality assurance procedures, guarantee a reliable, error-free app for your consumers.</li>
+                    <li><u>Transparent Communication:</u> Promote cooperation and trust by keeping you updated during the whole development process.</li> 
+                
+                </ul>
+               
                 </div>
 
             </div>
@@ -91,19 +103,23 @@
             <div class="row mt-5">
                 <div class="col-lg-8">
                     <h3 class="text-center">Graphics Designing</h3>
-                    <p>
-                        Rubicsol, a reputable software company, specialises in the comprehensive provision of RUBIC LIMS. Our persistent commitment to innovation and client-centric approach allows us to provide customised, secure, and user-friendly LIMS software.
-                    </p>
-                    <p>
-                        Through an emphasis on data quality, efficiency, and smooth integration, we enable our valued clients to optimise their laboratory processes, leading to automated Lab operations and overall increased output. We meticulously adhere to ISO/IEC 17025 standards keeping our clients stress-free for international recognition. Our dedication to quality, excellence and maintenance of long-time relationships with our clients has shaped our position as a leading player in the global LIMS marketplace.
-                    </p>
-                    <p>
-                        Discover the Rubicsol difference today and use our pioneering LIMS solution,the RUBIC LIMS to unlock the full potential of your lab. Become a part of our expanding clientele of happy customers and preserve your place in the cutthroat field of laboratory management.
-                    </p>
-                </div>
+                    <h5 class="text-center">Rubicsol’s Diverse Graphic Designing Services</h5>
+                    <p>Rubicsol's global network of elite graphic artists offer to create any kind of illustration you desire, stunning print designs, or captivating digital marketing materials. At Rubicsol, we think that the secret to enthralling your audience and enhancing the visibility of your business is outstanding graphic design. Our graphic design services realise your distinct demands, making sure that your marketing collateral, website, or app leaves a lasting impression.</p>
+                
+                    <ul>
+                
+                    <li><u>Business Identity Development:</u> We work with you to develop a unified visual language reflecting the values of your organisation.</li>
+                    <li><u>Consumer-Centric Design:</u> We put the user experience in our top priority, providing your app, website, or marketing materials with simple to use, and striking visuals.</li>
+                    <li><u>Design Approach:</u> We assist you stand out among competitors by using the newest design trends and technologies keeping you and us ahead of the curve.</li>
+                    <li><u>Collaborative Process:</u> Our designers collaborate with you closely to make sure your vision is realised keeping transparent communication under sincere consideration.</li>
+                    <li><u>Quality Proof:</u> We carefully examine and constantly improve our designs to make them as effective and high-quality as possible.</li>
+                
+                </ul>
+                
+            </div>
 
                 <div class="col-lg-4">
-                    <img src="{{url('images/about_us.png')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
+                    <img src="{{url('images/graphic-design.jpg')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
                 </div>
             </div>
         </div>
@@ -117,7 +133,7 @@
             <div class="row mt-5">
 
                 <div class="col-lg-4">
-                    <img src="{{url('images/our_vision.png')}}" alt="" class="img-fluid w-100">
+                    <img src="{{url('images/our_vision.png')}}" alt="" class="img-fluid w-100 mt-5 pt-4">
                 </div>
                 <div class="col-lg-8">
                     <h3 class="text-center">POS</h3>
