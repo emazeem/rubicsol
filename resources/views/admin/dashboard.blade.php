23e5ed4c13fbc7613bb3b14e646ea88b2e8bd393
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2024-04-02 12:46:23 +0500

    confilct-resolved

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -161,16 +161,9 @@
     </div>
   </div>
 
-<<<<<<< HEAD
 <!--calendar-->
 
     
-=======
-<!--calendar--> 
-    <div class="col-md-12">
-      <div id='calendar'></div>
-    </div>
->>>>>>> 0ba95587588c204d243f54cde4bf26a651478b5d
   </div>
   
   <!--Calendar-->
