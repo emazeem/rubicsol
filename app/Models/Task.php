0ba95587588c204d243f54cde4bf26a651478b5d
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2024-04-02 12:40:12 +0500

    lims

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,7 +12,7 @@ class Task extends Model
 {
     use HasFactory;
     public function user(){
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class,'user_id')->withDefault();
     }
     public function subtasks(){
         return $this->hasMany(Subtask::class,'task_id');
