40a99938edafefe62c54bd3f07f3327b1cf25eae
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2024-04-05 10:57:31 +0500

    .

--- resources/views/admin/attendance/index.blade.php
+++ resources/views/admin/attendance/index.blade.php
@@ -6,7 +6,7 @@
 <div class="row">
   <div class="col-12 mb-2">
     <h3 class="float-left pb-1 font-weight-light"><i class="feather icon-users"></i> Attendance</h3>
-    @if(auth()->user()->role=="super-admin")
+    @if(auth()->user()->role == "super-admin")
     <a href="{{route('attendance.create')}}" class="btn btn-primary shadow-sm float-right mt-2"><i class="fa fa-plus-circle mx-1"></i>Attendance</a>
     @endif
   </div>
@@ -38,32 +38,32 @@
       </tr>
     </thead>
     <tbody>
-      @if(count($attendances)>0)
+      @if(count($attendances) > 0)
       @foreach($attendances as $attendance)
       <tr class="table-row">
         <td>{{$attendance->id}}</td>
         <td>{{$attendance->user->fname}} {{$attendance->user->lname}}</td>
         <td>{{$attendance->check_in_date}}</td>
-        <td>{{date('h:i A',strtotime($attendance->check_in))}}</td>
+        <td>{{date('h:i A', strtotime($attendance->check_in))}}</td>
         <td>{{$attendance->check_out_date}}</td>
-        <td>{{date('h:i A',strtotime($attendance->check_out))}}</td>
+        <td>{{date('h:i A', strtotime($attendance->check_out))}}</td>
         <td>
-          @if($attendance->status==1)
+          @if($attendance->status == 1)
           <span class="badge badge-success">Check Out</span>
           @endif
-          @if($attendance->status==0)
+          @if($attendance->status == 0)
           <span class="badge badge-primary">Check In</span>
           @endif
         </td>
-        @if(auth()->user()->role=="super-admin")
+        @if(auth()->user()->role == "super-admin")
         <td>
-          <a href="{{route('attendance.edit',['id'=>$attendance->id])}}" class="btn btn-success btn-sm"><i class="fas fa-edit"></i></a></button>
-          <a href="{{route('attendance.show',['id'=>$attendance->id])}}" class="btn btn-warning btn-sm" ><i class="fas fa-eye"></i></a></button>
-          <a href="{{route('attendance.delete',['id'=>$attendance->id])}}" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
+          <a href="{{route('attendance.edit', ['id' => $attendance->id])}}" class="btn btn-success btn-sm"><i class="fas fa-edit"></i></a></button>
+          <a href="{{route('attendance.show', ['id' => $attendance->id])}}" class="btn btn-warning btn-sm" ><i class="fas fa-eye"></i></a></button>
+          <a href="{{route('attendance.delete', ['id' => $attendance->id])}}" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
         </td>
         @else
         <td class="text-center">
-            <a href="{{route('attendance.show',['id'=>$attendance->id])}}" class="btn btn-warning btn-sm" ><i class="fas fa-eye"></i></a></button>
+            <a href="{{route('attendance.show', ['id' => $attendance->id])}}" class="btn btn-warning btn-sm" ><i class="fas fa-eye"></i></a></button>
           </td>
         </tr>
         @endif
