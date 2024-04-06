57239674ec700cd3a6ab7147bd1f00703c3580aa
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2024-04-06 11:18:02 +0500

    tasks undo

--- resources/views/admin/tasks/index.blade.php
+++ resources/views/admin/tasks/index.blade.php
@@ -70,40 +70,5 @@
         background: #fff!important;
      }
      </style>
-<script type="text/javascript">
-$(document).on('click', '.delete', function (e) {
-  e.preventDefault();
-                swal({
-                    title: "Are you sure to delete this task?",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                    .then((willDelete) => {
-                        if (willDelete) {
-                            var id = $(this).attr('data-id');
-                            var token = '{{csrf_token()}}';
-                            e.preventDefault();
-                            $.ajax({
-                                url: "{{route('task.delete' , $tasks->id)}}",
-                                type: 'POST',
-                                dataType: "JSON",
-                                data: {id:id,_token:token},
-                                success: function (data) {
-                                    swal('success', data.success, 'success').then(function (){
-                                        location.reload();
-                                    });
-                                },
-                                error: function (xhr) {
-                                    erroralert(xhr);
-                                },
-                            });
 
-                        }
-                    });
-            });
-
-
-
-</script>
 @endsection
