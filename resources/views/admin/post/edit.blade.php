6b9e45d9c4aaddf4024f854a9daacf57dd86a49f
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2024-04-15 14:03:37 +0500

    lims

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -26,36 +26,32 @@
                 
               </div>
               
-              <div class="form-group col-md-4 col-12">
-                <label for="content" class="control-label">Content</label>
-                <textarea type="text"  rows="4" cols="50" class="form-control" id="content" name="content"
-                placeholder="Content" value="{{$edit->content}}">
-
-                </textarea>
-                <!-- <input type="text"  rows="6" cols="50" class="form-control" id="content" name="content"
-                placeholder="Content" value="{{$edit->content}}"> -->
-              </div> 
-              <!--image uploading-->
-                <div class="form-group ml-3">
-                  <label for="image">Choose Image</label>
-                  <input type="file" class="form-control-file" id="image" name="image" value="{{$edit->image}}">
-                </div>
-               
-              </div>
-                <div class="card-footer bg-light border-top">
-                    <div class="row">
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary btn-md user-btn float-right">
-                            <iclass="feather icon-save></i>Save
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          </form>
-            </div>
+        <div class="form-group col-md-4 col-12">
+          <label for="content" class="control-label">Content</label>
+          <textarea type="text"  rows="4" cols="50" class="form-control" id="content" name="content"
+          placeholder="Content" value="{{$edit->content}}"></textarea>
         </div>
-        <script type="text/javascript">
+        <!--image uploading-->
+        <div class="form-group ml-3">
+          <label for="image">Choose Image</label>
+          <input type="file" class="form-control-file" id="image" name="image" value="{{$edit->image}}">
+        </div>
+      </div>
+      <div class="card-footer bg-light border-top">
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-md user-btn float-right">
+              <i class="feather icon-save"></i>Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
+</div>
+        
+<script type="text/javascript">
  $(document).ready(function () {
     $("#user-form").on('submit', (function (e) {
         var button = $('.user-btn');
